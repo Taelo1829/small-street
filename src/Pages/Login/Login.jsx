67b0738b3c7e04146fbd64c32d760f36a7a724a9
Taelo <taelo@izon.group>
2026-03-12 19:45:58 +0200

    added a lot of input fields

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Logo from './Logo'
+import Input from '../../Components/Input'
+import Button from '../../Components/Button'
+import Google from './Google'
 
 const Login = () => {
     return (
         <div className="login">
-            <Logo />
+            <div>
+                <Logo />
+            </div>
+            <div>
+                <Google />
+                <Input label="Username, email or mobile number" />
+                <Input label="Password" />
+                <Button value="Log in" />
+                <div className='forgot-password'>
+                    <text >Forgot password?</text>
+                </div>
+            </div>
         </div>
     )
 }
