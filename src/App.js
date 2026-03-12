453786a5f941e80e098006f49e6cde422fa3894e
Taelo <taelo@izon.group>
2026-03-12 18:44:18 +0200

    designed logo for the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login';
+import Login from './Pages/Login/Login';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <Logo />
+        </div>
+    )
+}
+
+export default Login
