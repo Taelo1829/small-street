aa175e09211fa76ed2950a8e22ad26749ee7f9fc
Taelo <taelo@izon.group>
2026-03-12 18:30:37 +0200

    working on the login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center min-h-screen'>
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                hello world
+            </div>
+        </div>
+    )
+}
+
+export default Login
